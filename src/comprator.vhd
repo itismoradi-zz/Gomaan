f936cbdae5f114ec0a6160d719080d94c5447ce8
Mohammad Moradi <itismoradi@gmail.com>
2021-02-22 23:35:36 +0330

    edit comparator architecture

--- src/comprator.vhd
+++ src/comprator.vhd
@@ -2,12 +2,14 @@ LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
 
 ENTITY comparator IS
-    PORT (
-        a : IN STD_LOGIC_VECTOR (3 DOWNTO 0);
-        b : IN STD_LOGIC_VECTOR (3 DOWNTO 0);
-        less : OUT STD_LOGIC;
-        correct : OUT STD_LOGIC;
-        upper : OUT STD_LOGIC);
+    PORT 
+    (
+        a     : IN  STD_LOGIC_VECTOR (3 DOWNTO 0);
+        b     : IN  STD_LOGIC_VECTOR (3 DOWNTO 0);
+        less  : OUT STD_LOGIC;
+        equal : OUT STD_LOGIC;
+        upper : OUT STD_LOGIC
+    );
 END comparator;
 
 ARCHITECTURE Behavioral OF comparator IS
@@ -16,19 +18,18 @@ BEGIN
 
     PROCESS (a, b)
     BEGIN
+        less <= '0';
+        correct <= '0';
+        upper <= '0';
+
         IF b < a THEN
             less <= '1';
-            correct <= '0';
-            upper <= '0';
         ELSIF b = a THEN
-            less <= '0';
-            correct <= '1';
-            upper <= '0';
+            equal <= '1';
         ELSIF b > a THEN
-            less <= '0';
-            correct <= '0';
             upper <= '1';
         END IF;
+        
     END PROCESS;
 
 END Behavioral;
