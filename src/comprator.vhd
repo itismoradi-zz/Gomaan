9a5364991d9af5db3e0f988f6fbfd02f7a231a27
Amir Tavana <amirmahdirezaetavana@gmail.com>
2021-02-22 16:08:51 +0330

    omplete comprative

--- src/comprator.vhd
+++ src/comprator.vhd
@@ -1,19 +1,34 @@
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
--- first comprator---
-ENTITY ang_gat IS
+
+ENTITY comparator IS
     PORT (
-        A, B : IN BIT;
-        L, G, E : OUT BIT
-    );
+        a : IN STD_LOGIC_VECTOR (3 DOWNTO 0);
+        b : IN STD_LOGIC_VECTOR (3 DOWNTO 0);
+        less : OUT STD_LOGIC;
+        correct : OUT STD_LOGIC;
+        upper : OUT STD_LOGIC);
+END comparator;
 
-END ang_gat;
+ARCHITECTURE Behavioral OF comparator IS
 
-ARCHITECTURE Behavioral OF ang_gat IS
-    SIGNAL A_not, B_not : BIT;
 BEGIN
-    g <= a AND (NOT B);
-    l <= (NOT a) AND B;
-    E <= A XNOR B;
+
+    PROCESS (a, b)
+    BEGIN
+        IF b < a THEN
+            less <= '1';
+            correct <= '0';
+            upper <= '0';
+        ELSIF b = a THEN
+            less <= '0';
+            correct <= '1';
+            upper <= '0';
+        ELSIF b > a THEN
+            less <= '0';
+            correct <= '0';
+            upper <= '1';
+        END IF;
+    END PROCESS;
 
 END Behavioral;
